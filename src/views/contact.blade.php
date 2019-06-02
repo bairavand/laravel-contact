fce4b4feb461989fbb289f094f17a27795b623c7
pairavanvvl <36874538+pairavanvvl@users.noreply.github.com>
2019-06-02 22:23:51 +0530

    Add files via upload

--- src/views/contact.blade.php
+++ src/views/contact.blade.php
@@ -8,28 +8,30 @@
     <title>Contact Us</title>
 </head>
 <body>
-    <div class="card uper">
-        <div class="card-header">
-            Contact Us
-        </div>
-        <div class="card-body">
-            <form action="{{route('contact')}}" method="post">
-                @csrf
-                <div class="form-group">
-                    <label for="name">Name:</label>
-                    <input type="text" class="form-control" name="name" placeholder="Your Name Please">
-                </div>
-                <div class="form-group">
-                    <label for="email">Email:</label>
-                    <input type="email" class="form-control" name="email" placeholder="Your Valid Email">
-                </div>
-                <div class="form-group">
-                    <label for="message">Message:</label>
-                    <textarea name="message" class="form-control" cols="30" rows="10" placeholder="Your Query"></textarea>
-                </div>
-                <button type="submit" class="btn btn-primary"> Send </button>
-            </form>
-        </div>
+    <div style="margin:50px 400px 0 400px;">
+        <div class="card uper">
+            <div class="card-header">
+                Contact Us
+            </div>
+            <div class="card-body">
+                <form action="{{route('contact')}}" method="post">
+                    @csrf
+                    <div class="form-group">
+                        <label for="name">Name:</label>
+                        <input type="text" class="form-control" name="name" placeholder="Your Name Please">
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email:</label>
+                        <input type="email" class="form-control" name="email" placeholder="Your Valid Email">
+                    </div>
+                    <div class="form-group">
+                        <label for="message">Message:</label>
+                        <textarea name="message" class="form-control" cols="30" rows="10" placeholder="Your Query"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary"> Send </button>
+                </form>
+            </div>
+        </div>  
     </div>
 </body>
 </html>
